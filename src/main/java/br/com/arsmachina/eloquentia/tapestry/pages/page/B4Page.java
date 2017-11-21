18053532ca3b59644b238cbe16e2215802475e4e
max <mparentini@dotcom.ts.it>
2017-11-21 17:52:43 +0100

    basic example

---
+++ src/main/java/br/com/arsmachina/eloquentia/tapestry/pages/page/B4Page.java
@@ -0,0 +1,8 @@
+package br.com.arsmachina.eloquentia.tapestry.pages.page;
+
+import org.apache.tapestry5.annotations.Import;
+
+@Import(stylesheet={"context:css/starter-template.css"}, library={"context:js/tether.min.js"}, stack={"core"})
+public class B4Page {
+
+}
